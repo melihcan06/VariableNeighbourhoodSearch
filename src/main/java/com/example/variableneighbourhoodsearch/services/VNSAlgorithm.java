4ca690de795408b8a96e5fdf6e60b6332d82076f
Melih Can <melihcankilic@gmail.com>
2023-01-07 18:27:15 +0300

    second

--- src/main/java/com/example/variableneighbourhoodsearch/services/VNSAlgorithm.java
+++ src/main/java/com/example/variableneighbourhoodsearch/services/VNSAlgorithm.java
@@ -33,51 +33,57 @@ public class VNSAlgorithm {
     //allLabels -> graphLabels; cLabels -> subGraphLabels, C(feasible sol.) labels in algorithm
     public HashSet<String> shakingPhase(HashSet<String> allLabels, HashSet<String> cLabels, int k) {
         HashSet<String> c2Labels = (HashSet<String>) cLabels.clone();
+        System.out.println("shakingPhase baslangic");
+        c2Labels.forEach(s1 -> System.out.println(s1));
+        System.out.println("---");
         for (int i = 0; i < k; i++) {
-            if (Math.random() <= 0.5) {
+            if (Math.random() <= 0.5 && c2Labels.size() > 2) {//islemi hizlandirmak icin 2. kontrol koyuldu
                 deleteLabelFromSubGraph(c2Labels);
             } else {
-                //System.out.println("---");
-                //c2Labels.forEach(s1 -> System.out.println(s1));
-                //System.out.println("--");
                 addUnusedLabelToGraph(allLabels, c2Labels);
-                //c2Labels.forEach(s1 -> System.out.println(s1));
-                //System.out.println("---");
             }
         }
+        c2Labels.forEach(s1 -> System.out.println(s1));
+        System.out.println("shakingPhase bitis");
         return c2Labels;
     }
 
     public Graph localSearch(Graph allGraph, Graph subGraph2) {
-        while (!subGraph2.isGraphConnected()) {
-
-            System.out.println("------BASLA-------");
+        System.out.println("localSearch baslangic");
+        subGraph2.getLabels().forEach(s1 -> System.out.println(s1));
+        System.out.println("---------------");
+        while (!(subGraph2.isGraphConnected() && subGraph2.isAllNodesUsing(allGraph, subGraph2))) {
+            //System.out.println("------BASLA-------");
 
             HashSet<String> c2 = subGraph2.getLabels();
-            c2.forEach(s1 -> System.out.println(s1));
-            System.out.println("---------------");
+            /*c2.forEach(s1 -> System.out.println(s1));
+            System.out.println("---------------");*/
 
             addUnusedLabelToGraph(allGraph.getLabels(), c2);
 
-            c2.forEach(s1 -> System.out.println(s1));
-            System.out.println("---------------");
+            /*c2.forEach(s1 -> System.out.println(s1));
+            System.out.println("---------------");*/
 
             subGraph2 = allGraph.createSubGraph(c2, false);
 
-            System.out.println("---------------");
+            /*System.out.println("---------------");
             subGraph2.getLabels().forEach(s1 -> System.out.println(s1));
 
-            System.out.println("------BITIR------");
+            System.out.println("------BITIR------");*/
         }
+        subGraph2.getLabels().forEach(s1 -> System.out.println(s1));
+        System.out.println("---------------");
         for (int i = 0; i < subGraph2.getLabels().size(); i++) {
             HashSet<String> c2 = subGraph2.getLabels();
             String deletedLabel = deleteLabelFromSubGraph(c2);
             subGraph2 = allGraph.createSubGraph(c2, false);
-            if (!subGraph2.isGraphConnected() && !deletedLabel.isBlank()) {
+            if (!(subGraph2.isGraphConnected() && subGraph2.isAllNodesUsing(allGraph, subGraph2)) && !deletedLabel.isBlank()) {
                 c2.add(deletedLabel);
                 subGraph2 = allGraph.createSubGraph(c2, false);
             }
         }
+        subGraph2.getLabels().forEach(s1 -> System.out.println(s1));
+        System.out.println("localSearch bitis");
         return subGraph2;
     }
 
@@ -86,12 +92,18 @@ public class VNSAlgorithm {
         System.out.println("Feasible Solution");
         subGraph.printGraphAll(graph);
 
-        for (int i = 0; i < 1; i++) {//TODO gecici olarak durma kosulu, degistir!
+        boolean isFinished = false;
+        //for (int i = 0; i < 1; i++) {//TODO gecici olarak durma kosulu, degistir!
+        while(!isFinished){
+            /*Graph subGraph = graph.generateInitialSolution();
+            System.out.println("Feasible Solution");
+            subGraph.printGraphAll(graph);*/
+
             int k = 1;
             int kMax = subGraph.getLabels().size() + subGraph.getLabels().size() / 3;
             while (k < kMax) {
                 HashSet<String> c2 = shakingPhase(graph.getLabels(), subGraph.getLabels(), k);
-                Graph subGraph2 = subGraph.createSubGraph(c2, false);
+                Graph subGraph2 = graph.createSubGraph(c2, false);
                 subGraph2 = localSearch(graph, subGraph2);
                 if (subGraph2.getLabels().size() < subGraph.getLabels().size()) {
                     subGraph = subGraph2.cloneGraph();
@@ -100,8 +112,9 @@ public class VNSAlgorithm {
                     k++;
                 }
             }
+            isFinished = subGraph.isGraphConnected() && subGraph.isAllNodesUsing(graph, subGraph);
+            /*System.out.println("Last Graph");
+            subGraph.printGraphAll(graph);*/
         }
-        System.out.println("Last Graph");
-        subGraph.printGraphAll(graph);
     }
 }
